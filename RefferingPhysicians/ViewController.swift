dbf84dd1e6ef6bbb98bb64adcc69f25d2b9c2dbc
dmgerdes <dmgerdes@hotmail.com>
2015-01-28 13:44:52 -0800

    Initial Commit

---
+++ RefferingPhysicians/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RefferingPhysicians
+//
+//  Created by Mike Gerdes on 1/28/15.
+//  Copyright (c) 2015 Virginia Mason Medical Center. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
